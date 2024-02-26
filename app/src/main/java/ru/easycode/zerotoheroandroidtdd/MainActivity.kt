fc8bf27f874d6b7fcfde1044bcacbc5cc19842a2
IvanUsatov <andrappdevelop@gmail.com>
2024-02-26 20:13:33 +0700

    task/015 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,35 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    private lateinit var textView: TextView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        progressBar = findViewById(R.id.progressBar)
+        button = findViewById(R.id.actionButton)
+
+        viewModel = (application as App).viewModel
+
+        button.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) {
+            it.apply(textView, progressBar, button)
+        }
     }
 }
